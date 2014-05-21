d5357a97a6fdc3f8295fbe2784ad7674a132e6e1
Bojan Antolovic <bojan.antolovic@gmail.com>
2014-05-21 15:34:41 +0200

    Improved build scripts.

--- Plugins/Rhetos.MvcModelGenerator.Test/Properties/AssemblyInfo.cs
+++ Plugins/Rhetos.MvcModelGenerator.Test/Properties/AssemblyInfo.cs
@@ -27,9 +27,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Rhetos.MvcModelGenerator.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Omega software")]
 [assembly: AssemblyProduct("Rhetos.MvcModelGenerator.Test")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright (C) Omega software 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.6.0.1")]
+[assembly: AssemblyFileVersion("1.6.0.1")]
