b8dbce145805a7a32189bae22971af83f962b920
Damir <ddean@omega-software.hr>
2019-03-22 14:54:40 +0100

    Added ImplementInMvcModel DSL concept.

--- Build.bat
+++ Build.bat
@@ -2,8 +2,13 @@ SETLOCAL
 SET Version=2.3.0
 SET Prerelease=auto
 
-IF NOT DEFINED VisualStudioVersion CALL "%VS140COMNTOOLS%VsDevCmd.bat" || ECHO ERROR: Cannot find Visual Studio 2015, missing VS140COMNTOOLS variable. && GOTO Error0
+@IF DEFINED VisualStudioVersion GOTO SkipVcvarsall
+@SET VSTOOLS=
+@IF EXIST "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat" SET VSTOOLS="%VS140COMNTOOLS%..\..\VC\vcvarsall.bat" x86
+@IF EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat" SET VSTOOLS="C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat" -arch=x86
+CALL %VSTOOLS% || GOTO Error0
 @ECHO ON
+:SkipVcvarsall
 
 PowerShell .\ChangeVersion.ps1 %Version% %Prerelease% || GOTO Error0
 WHERE /Q NuGet.exe || ECHO ERROR: Please download the NuGet.exe command line tool. && GOTO Error0
---
+++ Plugins/Rhetos.MvcModelGenerator.DefaultConcepts/DataStructures/ImplementsInterfaceCodeGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.Composition;
+using Rhetos.Compiler;
+using Rhetos.Dsl;
+using Rhetos.Dsl.DefaultConcepts;
+using Rhetos.Extensibility;
+
+namespace Rhetos.MvcModelGenerator.DefaultConcepts
+{
+	[Export(typeof(IConceptInfo))]
+	[ConceptKeyword("ImplementInMvcModel")]
+	public class ImplementInMvcModelInfo : IConceptInfo
+	{
+		[ConceptKey]
+		public ImplementsInterfaceInfo Implements { get; set; }
+	}
+
+	[Export(typeof(IMvcModelGeneratorPlugin))]
+	[ExportMetadata(MefProvider.Implements, typeof(ImplementInMvcModelInfo))]
+	public class ImplementsInterfaceCodeGenerator : IMvcModelGeneratorPlugin
+	{
+		public void GenerateCode(IConceptInfo conceptInfo, ICodeBuilder codeBuilder)
+		{
+			var info = (ImplementInMvcModelInfo)conceptInfo;
+			Dom.DefaultConcepts.DataStructureCodeGenerator.AddInterfaceAndReference(codeBuilder, info.Implements.GetInterfaceType(), info.Implements.DataStructure);
+		}
+	}
+}
