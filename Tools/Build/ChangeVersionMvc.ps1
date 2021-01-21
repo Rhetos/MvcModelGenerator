9971777dc5095f857dc5e61f253a084c86a02210
Ivan Barbančić <ibarbancic@omega-software.hr>
2021-01-21 14:31:19 +0100

    Added Directory.Build.props file

--- Tools/Build/ChangeVersionMvc.ps1
+++ Tools/Build/ChangeVersionMvc.ps1
@@ -25,7 +25,8 @@ function RegexReplace ($fileSearch, $replacePattern, $replaceWith)
 
 Write-Output "Setting version '$fullVersion'."
 
-RegexReplace '*AssemblyInfo.cs' '([\n^]\[assembly: Assembly(File)?Version(Attribute)?\(\").*(\"\)\])' ('${1}'+$version+'${4}')
-RegexReplace '*AssemblyInfo.cs' '([\n^]\[assembly: AssemblyInformationalVersion(Attribute)?\(\").*(\"\)\])' ('${1}'+$fullVersion+'${3}')
 RegexReplace '*.nuspec' '([\n^]\s*\<version\>).*(\<\/version\>\s*)' ('${1}'+$fullVersion+'${2}')
 RegexReplace 'Rhetos.MvcModelGenerator.nuspec' '(Rhetos.MvcModelGenerator.Client\" version=\").*(\")' ('${1}'+$fullVersion+'${2}')
+RegexReplace 'Directory.Build.props' '([\n^]\s*\<InformationalVersion\>).*(\<\/InformationalVersion\>\s*)' ('${1}' + $fullVersion + '${2}')
+RegexReplace 'Directory.Build.props' '([\n^]\s*\<AssemblyVersion\>).*(\<\/AssemblyVersion\>\s*)' ('${1}' + $version + '${2}')
+RegexReplace 'Directory.Build.props' '([\n^]\s*\<FileVersion\>).*(\<\/FileVersion\>\s*)' ('${1}' + $version + '${2}')
