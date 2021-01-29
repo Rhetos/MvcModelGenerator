44d602e91b54d66f498e920103682abc6004c66d
Bojan Antolović <bojan.antolovic@gmail.com>
2021-01-29 18:47:13 +0100

    Build.bat cleans output to avoid using newer version from different branch.

--- Build.bat
+++ Build.bat
@@ -7,10 +7,12 @@ CALL Tools\Build\FindVisualStudio.bat || GOTO Error0
 REM Updating the build version.
 PowerShell -ExecutionPolicy ByPass .\Tools\Build\ChangeVersionMvc.ps1 %Version% %Prerelease% || GOTO Error0
 
+IF NOT EXIST Install MD Install || GOTO Error0
+DEL /F /S /Q Install\* || GOTO Error0
+
 WHERE /Q NuGet.exe || ECHO ERROR: Please download the NuGet.exe command line tool. && GOTO Error0
 NuGet restore -NonInteractive || GOTO Error0
 MSBuild /target:rebuild /p:Configuration=Debug /verbosity:minimal /fileLogger || GOTO Error0
-IF NOT EXIST Install md Install
 NuGet pack Rhetos.MvcModelGenerator.Client.nuspec -OutputDirectory Install || GOTO Error0
 NuGet pack Rhetos.MvcModelGenerator.nuspec -OutputDirectory Install || GOTO Error0
 
