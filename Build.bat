bd38bc02f82dc8fa5813b84d83b33bca5d973084
Bojan Antolović <bojan.antolovic@gmail.com>
2022-02-03 22:19:55 +0100

    Consistent build scripts between Rhetos packages.

--- Build.bat
+++ Build.bat
@@ -2,15 +2,18 @@ SETLOCAL
 SET Version=5.0.0
 SET Prerelease=auto
 
+@SET Config=%1%
+@IF [%1] == [] SET Config=Debug
+
 REM Updating the build version.
 PowerShell -ExecutionPolicy ByPass .\Tools\Build\ChangeVersionMvc.ps1 %Version% %Prerelease% || GOTO Error0
 
-IF NOT EXIST Install\ MD Install
+dotnet build --configuration %Config% -p:RhetosDeploy=false || GOTO Error0
+
+IF NOT EXIST Install MD Install
 DEL /F /S /Q Install\* || GOTO Error0
 
 WHERE /Q NuGet.exe || ECHO ERROR: Please download the NuGet.exe command line tool. && GOTO Error0
-dotnet build "Rhetos.MvcModelGenerator.sln" --configuration Debug || GOTO Error0
-IF NOT EXIST Install md Install
 NuGet pack Rhetos.MvcModelGenerator.Client.nuspec -OutputDirectory Install || GOTO Error0
 NuGet pack Rhetos.MvcModelGenerator.nuspec -OutputDirectory Install || GOTO Error0
 
