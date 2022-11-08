63bd6c1ee3f25f869aae757e4d38769db02d5a90
Bojan Antolović <bojan.antolovic@gmail.com>
2022-11-08 14:59:00 +0100

    Updated LINQPad version.

--- Test.bat
+++ Test.bat
@@ -6,8 +6,8 @@ SETLOCAL
 @REM Using "no-build" option as optimization, because Test.bat should always be executed after Build.bat.
 dotnet test --no-build || GOTO Error0
 
-IF EXIST "%ProgramFiles%\LINQPad6\LPRun6-net5.exe" "%ProgramFiles%\LINQPad6\LPRun6-net5.exe" "Test\Test.linq" > nul || GOTO Error0       
-IF EXIST "%ProgramFiles%\LINQPad6\LPRun6-net5.exe" "%ProgramFiles%\LINQPad6\LPRun6-net5.exe" "Test\Test Captions.linq" > nul || GOTO Error0
+IF EXIST "%ProgramFiles%\LINQPad7\LPRun7.exe" "%ProgramFiles%\LINQPad7\LPRun7.exe" "Test\Test.linq" > nul || GOTO Error0
+IF EXIST "%ProgramFiles%\LINQPad7\LPRun7.exe" "%ProgramFiles%\LINQPad7\LPRun7.exe" "Test\Test Captions.linq" > nul || GOTO Error0
 
 @REM ================================================
 
